188ea32235e5ff3bae449f9d2de01b3af7494a7d
Jackson Jaensch <jackson.jaensch14@gmail.com>
2022-12-15 11:40:36 +1030

    Fix flash messages, both now appear

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,14 +3,20 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     @contact.request = request
     if @contact.deliver
-      flash[:success] = 'Message sent!'
+      flash[:success] = 'Message sent! Thankyou for your inquiry'
       redirect_to root_path
     else
-      flash[:error] = 'Could not send message'
+      flash.now[:error] = 'Could not send message, please check contact form'
       render 'pages/home', status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message, :nickname)
   end
 end
